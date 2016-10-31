925f3778ee88ed72ca15c701dd290acc9e8fc53c
Slav B <yborets@gmail.com>
2016-10-30 22:17:00 -0700

    urs() + TimeZon

--- clock/js/main.js
+++ clock/js/main.js
@@ -24,13 +24,15 @@ function GetTime (TimeZone) {
 	var hours = today.getUTCHours() + TimeZone; 
 	var minutes = today.getUTCMinutes()
 	var seconds = today.getUTCSeconds()
+	var ampm = "PM";
 
+	if (hours > 12) {ampm = "PM"} else {ampm = "AM"} 
 	if (hours > 12) {hours = hours - 12 }
 	if (hours == 0) {hours = 12}
 	if (hours < 10) {hours = "0" + hours}
 	if (minutes < 10) {minutes = "0" + minutes;}
 	if (seconds < 10) {seconds = "0" + seconds;}
 
-	var TimeScring = hours + ":" + minutes + ":" + seconds
+	var TimeScring = hours + ":" + minutes + ":" + seconds + " " +ampm;
 	return TimeScring; 
 }
