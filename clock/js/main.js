1c0f58ab3b8b5a7e59e61d1dde8722eb157f7b87
Slav B <yborets@gmail.com>
2016-10-30 22:29:30 -0700

    fix

--- clock/js/main.js
+++ clock/js/main.js
@@ -7,7 +7,7 @@ var tokyo_time = document.getElementById("time-Tokyo");
 
 function UpdateClock () {
 
-	usa_time.innerHTML = GetTime(5);
+	usa_time.innerHTML = GetTime(17);
 	moscow_time.innerHTML = GetTime(3);
 	berlin_time.innerHTML = GetTime(1);
 	tokyo_time.innerHTML = GetTime(9);
@@ -24,15 +24,14 @@ function GetTime (TimeZone) {
 	var hours = today.getUTCHours() + TimeZone; 
 	var minutes = today.getUTCMinutes()
 	var seconds = today.getUTCSeconds()
-	var ampm = "PM";
-
-	if (hours > 12) {ampm = "PM"} else {ampm = "AM"} 
+	var ampm = "PM"
+	if (hours > 12) {ampm = "PM"} else {ampm = "AM"}
 	if (hours > 12) {hours = hours - 12 }
 	if (hours == 0) {hours = 12}
 	if (hours < 10) {hours = "0" + hours}
 	if (minutes < 10) {minutes = "0" + minutes;}
 	if (seconds < 10) {seconds = "0" + seconds;}
 
-	var TimeScring = hours + ":" + minutes + ":" + seconds + " " +ampm;
+	var TimeScring = hours + ":" + minutes + ":" + seconds + " "+ ampm;
 	return TimeScring; 
 }
