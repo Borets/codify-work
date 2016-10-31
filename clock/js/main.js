8b75c27ec8d4fe6b130056158155e21a853a77c1
Slav B <yborets@gmail.com>
2016-10-30 21:55:25 -0700

    Fixed Times

--- clock/js/main.js
+++ clock/js/main.js
@@ -6,25 +6,30 @@ var tokyo_time = document.getElementById("time-Tokyo");
 
 
 function UpdateClock () {
-	var today = new Date()
-	var hours = today.getHours()
-	var minutes = today.getMinutes()
-	var seconds = today.getSeconds()
 
-	if (hours > 12) {hours = hours - 12 }
-	if (hours == 0) {hours = 12}
-	if (minutes < 10) {minutes = "0" + minutes;}
-	if (seconds < 10) {seconds = "0" + seconds;}
-
-	var TimeScring = hours + ":" + minutes + ":" + seconds; 	
-	usa_time.innerHTML = TimeScring;
-	moscow_time.innerHTML = TimeScring;
-	berlin_time.innerHTML = TimeScring;
-	tokyo_time.innerHTML = TimeScring;
+	usa_time.innerHTML = GetTime(5);
+	moscow_time.innerHTML = GetTime(3);
+	berlin_time.innerHTML = GetTime(1);
+	tokyo_time.innerHTML = GetTime(9);
 }
 
 
 function Start(){
 	UpdateClock()
 	setInterval(UpdateClock,1000);
+}
+
+function GetTime (TimeZone) {
+	var today = new Date()
+	var hours = today.getUTCHours() + TimeZone; 
+	var minutes = today.getUTCMinutes()
+	var seconds = today.getUTCSeconds()
+
+	if (hours > 12) {hours = hours - 12 }
+	if (hours == 0) {hours = 12}
+	if (minutes < 10) {minutes = "0" + minutes;}
+	if (seconds < 10) {seconds = "0" + seconds;}
+
+	var TimeScring = hours + ":" + minutes + ":" + seconds
+	return TimeScring; 
 }
