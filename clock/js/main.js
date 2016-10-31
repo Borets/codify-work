d97d99b849ec1cdef5927eaa4de8bbca6a696ac7
Slav B <yborets@gmail.com>
2016-10-30 23:33:52 -0700

    removing AM/PM

--- clock/js/main.js
+++ clock/js/main.js
@@ -19,19 +19,18 @@ function Start(){
 	setInterval(UpdateClock,1000);
 }
 
-function GetTime (TimeZone) {
+function GetTime (offset) {
 	var today = new Date()
-	var hours = today.getUTCHours() + TimeZone; 
+	var hours = today.getUTCHours() + offset; 
 	var minutes = today.getUTCMinutes()
 	var seconds = today.getUTCSeconds()
-	var ampm = "PM"
-	if (hours > 12) {ampm = "PM"} else {ampm = "AM"}
+
 	if (hours > 12) {hours = hours - 12 }
 	if (hours == 0) {hours = 12}
 	if (hours < 10) {hours = "0" + hours}
 	if (minutes < 10) {minutes = "0" + minutes;}
 	if (seconds < 10) {seconds = "0" + seconds;}
 
-	var TimeScring = hours + ":" + minutes + ":" + seconds + " "+ ampm;
+	var TimeScring = hours + ":" + minutes + ":" + seconds;
 	return TimeScring; 
 }
